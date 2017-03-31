46ee52e3c6d0ff4f156fd1159e0ad3db68935fe4
Bogdan Nechita <talk.bogdan@gmail.com>
2017-03-31 12:28:32 +0300

    Initial Commit

---
+++ ColorMaker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorMaker
+//
+//  Created by Bogdan Nechita on 3/31/17.
+//  Copyright © 2017 Bogdan Nechita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
